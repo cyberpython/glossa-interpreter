2034430878da7c68b2914ad27b50940b35a41ed2
Georgios Migdos <cyberpython@gmail.com>
2012-07-06 19:54:21 +0300

    Slight fix for the build script.

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 cp build.xml build.xml.bkp
 cp nbproject/project.properties project.properties.bkp
 sed "s|file.reference.antlr3.jar=lib/antlr-3.2.jar|file.reference.antlr3.jar=/usr/share/java/antlr3.jar|" nbproject/project.properties > tmp.properties
-sed "s|file.reference.joptsimple.jar=lib/jopt-simple-3.2.jar|file.reference.joptsimple.jar=/usr/share/java/joptsimple.jar|" tmp.properties > tmp.properties
-mv tmp.properties nbproject/project.properties
+sed "s|file.reference.joptsimple.jar=lib/jopt-simple-3.2.jar|file.reference.joptsimple.jar=/usr/share/java/joptsimple.jar|" tmp.properties > tmp2.properties
+rm -f tmp.properties
+mv tmp2.properties nbproject/project.properties
 cp build-linux.xml build.xml
 ant clean jar #build the project
 mv build.xml.bkp build.xml
